23415ee3f71dd7d4ecf9e347153d678f97966aa5
sonu <sonuprajapat2398@gmail.com>
2025-04-14 15:00:18 +0530

    Created hero section and a reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./sections/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,54 @@
+import Button from "../components/Button";
+import { words } from "../constants";
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                        s
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into real projects</h1>
+              <h1>that deliver projects</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I’m Sonu, a Web developer.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See My Work"
+              icon="/images/arrow-down.svg"
+            />
+          </div>
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
