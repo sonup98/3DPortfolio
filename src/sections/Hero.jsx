815b24a8b9ddee10db391a7e32c3fa661f0393f0
sonu <sonuprajapat2398@gmail.com>
2025-04-14 19:49:59 +0530

    Minor Typo change

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
                 </span>
               </h1>
               <h1>into real projects</h1>
-              <h1>that deliver projects</h1>
+              <h1>that deliver Results</h1>
             </div>
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
               Hi, I’m Sonu, a Web developer.
